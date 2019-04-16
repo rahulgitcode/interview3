7676d189849c74c5037ed0b0b3678318a3e99754
rahulgitcode <rahulsbhoj@gmail.com>
2019-04-15 22:39:20 -0500

    Renamed a file

--- src/com/hmsy/dailycodingproblem/Daily37.java
+++ src/com/hmsy/dailycodingproblem/Daily37.java
@@ -1,6 +1,18 @@
 package com.hmsy.dailycodingproblem;
+/**
+ * 
+ * Daily Coding Problem: Problem #37 [Easy]
+ * 
+ * This problem was asked by Google. 
 
-public class DailyXX {
+The power set of a set is the set of all its subsets. Write a function that, given a set, generates its power set.
+
+For example, given the set {1, 2, 3}, it should return {{}, {1}, {2}, {3}, {1, 2}, {1, 3}, {2, 3}, {1, 2, 3}}.
+
+You may also use a list or array to represent a set.
+ *
+ */
+public class Daily37 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
